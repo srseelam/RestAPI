67f01b1cb272acad96ae008b50051ce3c81ee521
srseelam <seelam.srinivas@gmail.com>
2018-05-02 18:12:28 +0530

    Updated to get the JSON Array

--- src/test/java/com/qa/test/GetTest.java
+++ src/test/java/com/qa/test/GetTest.java
@@ -2,10 +2,14 @@ package com.qa.test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.swing.text.html.parser.Entity;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,7 +23,7 @@ public class GetTest extends AbstractClass {
 	RestClient client;
 	String url;
 	CloseableHttpResponse httpResponse;
-	
+
 	@BeforeMethod
 	public void setup() {
 		base = new AbstractClass();
@@ -27,21 +31,21 @@ public class GetTest extends AbstractClass {
 
 	}
 
-	//Test case without headers
+	// Test case without headers
 	@Test
 	public void verifyuserInfo() throws ClientProtocolException, IOException {
 		client = new RestClient();
 		httpResponse = client.get(url);
-		
+
 		int statusCode = httpResponse.getStatusLine().getStatusCode();
 		System.out.println("Status code---->" + statusCode);
 		String stringBody = EntityUtils.toString(httpResponse.getEntity());
 		JSONObject jsonBody = new JSONObject(stringBody);
 		System.out.println("Json Body ---->" + jsonBody);
-		
+
 		String totalValue = Library.getValueOfJSON(jsonBody, "/total");
-		System.out.println("Value of total is ---->" +  totalValue);
-		
+		System.out.println("Value of total is ---->" + totalValue);
+
 		String firstName = Library.getValueOfJSON(jsonBody, "/data[0]/first_name");
 		String lastName = Library.getValueOfJSON(jsonBody, "/data[0]/last_name");
 		String id = Library.getValueOfJSON(jsonBody, "/data[0]/id");
@@ -52,28 +56,27 @@ public class GetTest extends AbstractClass {
 		System.out.println("Value of id  ---> " + id);
 		System.out.println("Value of avatar ---> " + avatar);
 
-		
 	}
-	
-	
-	//Test case with headers
+
+	// Test case with headers
 	@Test
-	public void getResponseWithHeader() throws ClientProtocolException, IOException{
+	public void getResponseWithHeader() throws ClientProtocolException, IOException {
 		HashMap<String, String> headerMap = new HashMap<String, String>();
 		client = new RestClient();
-		headerMap.put("Content-Type" , "application/json");
-		
+		headerMap.put("Content-Type", "application/json");
+
 		httpResponse = client.get(url, headerMap);
-		
+
 		int statusCode = httpResponse.getStatusLine().getStatusCode();
 		System.out.println("Status code---->" + statusCode);
 		String stringBody = EntityUtils.toString(httpResponse.getEntity());
 		JSONObject jsonBody = new JSONObject(stringBody);
 		System.out.println("Json Body ---->" + jsonBody);
-		
+
+	
 		String totalValue = Library.getValueOfJSON(jsonBody, "/total");
-		System.out.println("Value of total is ---->" +  totalValue);
-		
+		System.out.println("Value of total is ---->" + totalValue);
+
 		String firstName = Library.getValueOfJSON(jsonBody, "/data[0]/first_name");
 		String lastName = Library.getValueOfJSON(jsonBody, "/data[0]/last_name");
 		String id = Library.getValueOfJSON(jsonBody, "/data[0]/id");
@@ -83,6 +86,34 @@ public class GetTest extends AbstractClass {
 		System.out.println("Value of Last Name ---> " + lastName);
 		System.out.println("Value of id  ---> " + id);
 		System.out.println("Value of avatar ---> " + avatar);
+
+	}
+	
+	@Test
+	public void parseTheJSON() throws ClientProtocolException, IOException{
 		
+		HashMap<String, String> headerMap = new HashMap<String, String>();
+		client = new RestClient();
+		headerMap.put("Content-Type", "application/json");
+
+		httpResponse = client.get(url, headerMap);
+
+		
+		String stringBody = EntityUtils.toString(httpResponse.getEntity());
+		JSONObject jsonBody = new JSONObject(stringBody);
+		System.out.println("Json Body ---->" + jsonBody);
+		
+		
+		System.out.println("=======================================");
+		System.out.println("Json value = " + jsonBody.get("total"));
+		JSONArray obj;
+		obj = jsonBody.getJSONArray("data");
+		for (int i = 0; i < obj.length(); i++) {
+			System.out.println("json arry = " + obj.getJSONObject(i));
+		}
+		
+		System.out.println("===========================================");
+		
+
 	}
 }
